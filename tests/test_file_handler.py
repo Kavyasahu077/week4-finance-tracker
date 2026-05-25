7f07b272b3e481ef829473af28bd9b0c3c9a6cbf
Kavyasahu077 <kavyasahu.0712@gmail.com>
2026-05-25 17:41:40 +0530

    Add files via upload

---
+++ tests/test_file_handler.py
@@ -0,0 +1,3 @@
+from finance_tracker.file_handler import load_expenses
+
+print(load_expenses())
---
+++ tests/test_reports.py
@@ -0,0 +1,8 @@
+from finance_tracker.reports import show_statistics
+
+expenses = [
+    {"amount": 100, "category": "Food"},
+    {"amount": 200, "category": "Travel"}
+]
+
+show_statistics(expenses)
